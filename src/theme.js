1b128250af82098c955193d5d89113dc71f858b1
tektechnologies <info@tektechnologies.com>
2023-11-21 15:32:49 -0600

    add material ui typography and h1

--- src/theme.js
+++ src/theme.js
@@ -170,4 +170,33 @@ export const themeSettings = (mode) => {
               },
             }),
     },
- 
+    typography: {
+      fontFamily: ["Open Sans", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Open Sans", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Open Sans", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Open Sans", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Open Sans", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Open Sans", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Open Sans", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    }
+  };
+};
