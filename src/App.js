c6214cd579683cd8ecc7a810889000a643c5c047
tektechnologies <info@tektechnologies.com>
2023-11-27 11:43:11 -0600

    add routing for dashboard components to app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import Logout from "./components/Logout";
 import Profile from "./components/Profile";
 import DashBoard from "./pages/dashBoard/DashBoardHome";
 import HomePage from "./pages/HomePage";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import Calendar from "./scenes/calendar/calendar";
+
+
+
 
 
 import {
@@ -21,7 +34,19 @@ const router = createBrowserRouter(
       <Route path="profile" element={<Profile />} />
       <Route path="login" element={<Login />} />
       <Route path="logout" element={<Logout />} />
-      <Route path="dashboard" element={<DashBoard />} />
+
+      <Route path="dashboard" element={<DashBoard />}>
+        <Route path="/team" element={<Team />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="/bar" element={<Bar />} />
+        <Route path="/pie" element={<Pie />} />
+        <Route path="/line" element={<Line />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/geography" element={<Geography />} />
+      </Route>
     </Route>
   )
 );
--- src/pages/dashBoard/DashBoardHome.js
+++ src/pages/dashBoard/DashBoardHome.js
@@ -33,29 +33,24 @@ import { ColorModeContext, useMode } from "../../theme.js";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import TopBar from '../controlBars/TopBar.js';
 import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
-import Calendar from "./scenes/calendar/calendar";
 
 
 function DashBoardHome() {
   const [theme, colorMode] = useMode();
+
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
       <CssBaseline />
         <section className="app">
+        <Sidebar isSidebar={isSidebar} />
           <main className="content">
           <TopBar />
+         
+          
+          
+       
             <h1>Secret Cats!</h1>
           </main>
         </section>
