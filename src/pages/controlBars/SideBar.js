822a45ac2ed66b5cdd2e052c3887f21d6b027b57
tektechnologies <info@tektechnologies.com>
2023-11-27 21:58:56 -0600

    uncomment imports for links and update L for Link component in Item arrow function

--- src/pages/controlBars/SideBar.js
+++ src/pages/controlBars/SideBar.js
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 import "react-pro-sidebar/dist/css/styles.css";
 import { tokens } from "../../theme.js";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-// import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-// import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
-// import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
 // import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 // import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
 // import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
@@ -30,7 +30,7 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
       icon={icon}
     >
       <Typography>{title}</Typography>
-      <link to={to} />
+      <Link to={to} />
     </MenuItem>
   );
 };
@@ -120,9 +120,28 @@ const SideBar = () => {
             >
               dotData
             </Typography>
-            <Item />
-            <Item />
-            <Item />
+            <Item    title="Manage Team"
+              to="/team"
+              icon={<PeopleOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Contacts Information"
+              to="/contacts"
+              icon={<ContactsOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Invoices Balances"
+              to="/invoices"
+              icon={<ReceiptOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+
             <Typography
               variant="h6"
               color={colors.grey[300]}
@@ -133,7 +152,9 @@ const SideBar = () => {
             <Item />
             <Item />
             <Item />
-            <Typography></Typography>
+            <Typography>
+
+            </Typography>
             <Item />
             <Item />
             <Item />
