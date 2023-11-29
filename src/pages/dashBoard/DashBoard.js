34914f13ba515a84d0735d27ead7a9794501a561
tektechnologies <info@tektechnologies.com>
2023-11-29 10:44:25 -0600

    add statbox for email

--- src/pages/dashBoard/DashBoard.js
+++ src/pages/dashBoard/DashBoard.js
@@ -3,7 +3,7 @@ import { Box, Button,  useTheme } from "@mui/material"; //IconButton, Typography
 import { tokens } from "../../theme";
 // import { mockTransactions } from "../../data/mockData";
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
-// import EmailIcon from "@mui/icons-material/Email";
+import EmailIcon from "@mui/icons-material/Email";
 // import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 // import PersonAddIcon from "@mui/icons-material/PersonAdd";
 // import TrafficIcon from "@mui/icons-material/Traffic";
@@ -11,7 +11,7 @@ import Header from "../../components/Header";
 // import LineChart from "../../components/LineChart";
 // import GeographyChart from "../../components/GeographyChart";
 // import BarChart from "../../components/BarChart";
-// import StatBox from "../../components/StatBox";
+import StatBox from "../../components/StatBox.js";
 // import ProgressCircle from "../../components/ProgressCircle";
 
 const DashBoard = () => {
@@ -40,7 +40,36 @@ const DashBoard = () => {
         </Box>
       </Box>
           {/* add grid with charts  */}
+          <Box 
+            display="grid"
+            gridTemplateColumns="repeat(12, 1fr)"
+            gridAutoRows="140px"
+            gap="20px"
+          >
+          {/* row 1 dash board items  */}
+          <Box
+            gridColumn="span 3"
+            backgroundColor={colors.primary[400]}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          > 
+          <StatBox 
+          title="12,420"
+          subtitle="Emails Sent"
+          progress="0.75"
+          increase="+14"
+          icon={
+            <EmailIcon
+            sx={{color: colors.greenAccent[600], fontSize: "26px"}}
+            />
+          }
+          />
           
+          </Box>
+
+            
+          </Box>
 
 
 
