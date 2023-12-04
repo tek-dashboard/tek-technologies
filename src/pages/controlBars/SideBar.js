a5208afdfbe7eacfbb95b42fd4ceda7153642a9d
tektechnologies <info@tektechnologies.com>
2023-12-04 10:16:14 -0600

    rename folder, add POS icon to dashboard

--- src/pages/controlBars/SideBar.js
+++ src/pages/controlBars/SideBar.js
@@ -16,7 +16,7 @@ import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutl
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
-import userPic from '../../assests/CRAIGB.jpg'
+import userPic from '../../assets/CRAIGB.jpg'
 
 const Item = ({ title, to, icon, selected, setSelected }) => {
   const theme = useTheme();
