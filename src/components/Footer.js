48b9deeab6a800ab5a5eeda8ffbdfea1bdadb800
tektechnologies <info@tektechnologies.com>
2023-12-21 11:16:55 -0600

    remove placeholder paragraph

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,19 +7,7 @@ class Footer extends React.Component {
       <>
         <footer>
           <h1>Footer</h1>
-          <p>
-            Caramels sugar plum jelly sweet roll liquorice toffee. Ice cream
-            jelly candy croissant cookie topping soufflé cheesecake. Carrot cake
-            brownie tiramisu pudding gummies chocolate. Danish pastry apple pie
-            pie cake liquorice oat cake. Apple pie caramels powder sweet carrot
-            cake biscuit toffee. Gummi bears candy bonbon jelly cheesecake
-            pastry lemon drops macaroon. Shortbread jelly chocolate bar candy
-            pastry oat cake jelly-o. Cookie sesame snaps jelly-o chocolate bear
-            claw shortbread icing. Danish sweet roll marzipan lemon drops donut
-            wafer. Cheesecake toffee marzipan cake apple pie. Liquorice jujubes
-            wafer brownie pastry. Caramels gingerbread powder fruitcake
-            croissant.
-          </p>
+       
         </footer>
       </>
     );
