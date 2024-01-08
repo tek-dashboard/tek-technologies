371f8b3f84720dcdf29db5e458c78ebe3cf2183c
tektechnologies <info@tektechnologies.com>
2024-01-08 16:51:50 -0600

    add axios bootstrap and form event to state

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,92 @@
 import React from "react";
 import Footer from "../components/Footer";
-
+import Card from "react-bootstrap/Card";
+import axios from "axios";
 
 class Home extends React.Component {
 
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: "",
+      cityData: {},
+      lat: "",
+      lon: "",
+      mapImage: "",
+      displayError: false,
+      errorMessage: "",
+    };
+  }
+
+  handleCityInput = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      city: event.target.value,
+    });
+  };
+
+
+  //add getLocationData
+  submitCityHandler = async (event) => {
+    event.preventDefault();
+   try{
+    let URL = `process.env.REACT_APP_SERVER}&q=${this.state.city}&format=json`;
+    let cityInfo = await axios.get(URL);
+    let lat = cityInfo.data[0].lat;
+    let lon = cityInfo.data[0].lon;
+    let mapImage = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${cityInfo.data[0].lat},${cityInfo.data[0].lon}&zoom=13&size=1000x1000`;
+    console.log('1. Response object:', cityInfo.data[0]);
+    this.setState({
+      cityData: cityInfo.data[0],
+      lat: lat,
+      lon: lon,
+      mapImage: mapImage,
+      displayError: false,
+    });
+    this.displayWeather(lat, lon);
+  } catch(error) {
+    console.log("ERROR.message", error.message);
+    this.state({
+      displayError: true,
+      errorMessage: `An error occurred: ${error.response.status}`,
+    });
+  }
+};
   
   render() {
     return (
       <>
         <div>WELCOME HOME</div>
-       
+        <form onSubmit={this.submitCityHandler}>
+          <label>
+            Pick a City:
+            <input type="text" onChange={this.handleCityInput} />
+          </label>
+          <button type="submit">Explore!</button>
+        </form>
+        {this.state.error ? (
+          <p>{this.state.errorMessage}</p>
+        ) : (
+          // <p>{this.state.cityData}</p>
+          <>
+            {/* // <p>{this.state.cityData}</p> */}
+            <Card className="card" style={{ width: "18rem" }}>
+              <Card.Img
+                className="cardImage"
+                variant="top"
+                src={this.state.mapImage}
+              />
+              <Card.Body>
+                <Card.Title>{this.state.cityData.display_name}</Card.Title>
+                <Card.Text>
+                  {this.state.cityData.lat} , {this.state.cityData.lon}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            </>
+        )}
         <Footer />
       </>
     );
