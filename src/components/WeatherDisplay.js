57a066e6ddd8cdc123b7e0d2de07d4e139105b78
tektechnologies <info@tektechnologies.com>
2024-03-20 14:39:48 -0500

    remove li items format weather component

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,38 +1,18 @@
 import React, { Component } from "react";
 import { Accordion } from "react-bootstrap";
 
-
-
-
 class WeatherDisplay extends Component {
   render() {
     console.log("weather from state", this.props.dayForecast);
 
     return (
       <>
-        {/* <ul>
-          <li>DATE: {this.props.dayForecast.time}</li>
-          <li>FORECAST{this.props.dayForecast.forecast}</li>
-
-          <li>
+        <Accordion.Item eventKey={this.props.index}>
+          <Accordion.Header>
+            DATE: {this.props.dayForecast.time}
+          </Accordion.Header>
+          <Accordion.Body>
             <img
-              src={this.props.dayForecast.icon}
-              height="50px"
-              width="50px"
-              alt={"weather icon"}
-            />
-          </li>
-          <li>{this.props.dayForecast.moon}</li>
-        </ul> */}
-
-        
-
-
-       
-      <Accordion.Item eventKey={this.props.index}>
-        <Accordion.Header>DATE: {this.props.dayForecast.time}</Accordion.Header>
-        <Accordion.Body>
-        <img
               src={this.props.dayForecast.icon}
               height="50px"
               width="50px"
@@ -40,11 +20,8 @@ class WeatherDisplay extends Component {
             />
             FORECAST: {this.props.dayForecast.forecast}
             MOON: {this.props.dayForecast.moon}
-          
-        </Accordion.Body>
-      </Accordion.Item>
-     
-    
+          </Accordion.Body>
+        </Accordion.Item>
       </>
     );
   }
