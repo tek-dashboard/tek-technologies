8e05eb7f5b7490a280a71294eadf57c40ecb7206
tektechnologies <info@tektechnologies.com>
2024-03-22 11:10:39 -0500

    add flex to body for forecast view

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -7,57 +7,31 @@ class WeatherDisplay extends Component {
 
     return (
       <>
-       <Accordion defaultActiveKey={['0']} alwaysOpen>
-                  {/* {this.state.weatherToDisplay.map((dayForecast, index) => ( */}
-        {this.props.dayForecast.map((dayForecast, index) => (
-          <Accordion.Item eventKey={index}>
-            <Accordion.Header>DATE: {dayForecast.time}</Accordion.Header>
-            <Accordion.Body>
-              <img
-                src={dayForecast.icon}
-                height="50px"
-                width="50px"
-                alt={"weather icon"}
-              />
-              <ul
+        <Accordion defaultActiveKey={["0"]} alwaysOpen>
+          {this.props.dayForecast.map((dayForecast, index) => (
+            <Accordion.Item eventKey={index}>
+              <Accordion.Header>DATE: {dayForecast.time}</Accordion.Header>
+              <Accordion.Body
                 style={{
-                  float: "right",
-                  marginRight: "200px",
+                  display: "flex",
+                  alignItems: "flex-start",
+                  // justifyContent: "center",
                 }}
               >
-                <li>FORECAST: {dayForecast.forecast}</li>
-                <li>MOON: {dayForecast.moon}</li>
-              </ul>
-            </Accordion.Body>
-          </Accordion.Item>
-        ))}
+                <img
+                  src={dayForecast.icon}
+                  // height="50px"
+                  // width="50px"
+                  alt={"weather icon"}
+                />
+                <ul>
+                  <li>FORECAST: {dayForecast.forecast}</li>
+                  <li>MOON: {dayForecast.moon}</li>
+                </ul>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
         </Accordion>
-        {/* <Accordion.Item eventKey={this.props.index}>
-          <Accordion.Header>
-            DATE: {this.props.dayForecast.time}
-          </Accordion.Header>
-          <Accordion.Body
-          
-          >
-            <img
-              src={this.props.dayForecast.icon}
-              height="50px"
-              width="50px"
-              alt={"weather icon"}
-            />
-            <ul
-             style={{
-                  float: "right",
-                  marginRight: "200px"
-                }}
-            >
-              <li>FORECAST: {this.props.dayForecast.forecast}</li>
-              <li>MOON: {this.props.dayForecast.moon}</li>
-            </ul>
-            
-            
-          </Accordion.Body>
-        </Accordion.Item> */}
       </>
     );
   }
