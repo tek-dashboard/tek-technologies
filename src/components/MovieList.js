7996dab68b548c6b25ccea7a5db15df7d675f1a7
tektechnologies <info@tektechnologies.com>
2024-04-03 23:35:58 -0500

    images rendering need styling

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,11 +4,20 @@ export class MovieList extends React.Component {
   render() {
     // console.log('ppprops', this.props.movieList);
     return (
-     
       <>
+        <ul>
           {this.props.movieList.map((movie, index) => (
-       <h6 key={index}>{movie.title}</h6>
-         ))}
+            <>
+              <li key={index}>
+                <h6>{movie.title}</h6>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${movie.src}`}
+                  alt={this.props.title}
+                />
+              </li>
+            </>
+          ))}
+        </ul>
       </>
     );
   }
