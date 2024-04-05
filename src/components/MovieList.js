412fc291f04e2ecf19d778adf3f1de12edc46fa4
tektechnologies <info@tektechnologies.com>
2024-04-04 19:45:03 -0500

    add image sizing and template react bootstrap carousel

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-lone-blocks */
 import React from "react";
 
 export class MovieList extends React.Component {
@@ -13,6 +14,9 @@ export class MovieList extends React.Component {
                 <img
                   src={`https://image.tmdb.org/t/p/w500/${movie.src}`}
                   alt={this.props.title}
+                  height={"75px"}
+                  width={"75px"}
+                
                 />
               </li>
             </>
@@ -24,3 +28,31 @@ export class MovieList extends React.Component {
 }
 
 export default MovieList;
+
+{/** 
+<Carousel>
+<Carousel.Item>
+  <ExampleCarouselImage text="First slide" />
+  <Carousel.Caption>
+    <h3>First slide label</h3>
+    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+  </Carousel.Caption>
+</Carousel.Item>
+<Carousel.Item>
+  <ExampleCarouselImage text="Second slide" />
+  <Carousel.Caption>
+    <h3>Second slide label</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </Carousel.Caption>
+</Carousel.Item>
+<Carousel.Item>
+  <ExampleCarouselImage text="Third slide" />
+  <Carousel.Caption>
+    <h3>Third slide label</h3>
+    <p>
+      Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+    </p>
+  </Carousel.Caption>
+</Carousel.Item>
+</Carousel>
+*/}
